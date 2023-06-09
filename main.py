3a44b727e1b293b2b2ed91d4c84ed8ad3e513dad
AlexandreLrq <github@alexandre-l.fr>
2023-06-09 14:54:08 +0200

    MAJ de la liste des alias à chaque création ou suppression d'alias et gestion des erreurs lors de la création d'un alias qui existe déjà

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import ovh
 import datetime
 
+from ovh import ResourceConflictError
+
 import config
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -16,33 +18,10 @@ domaine = config.DOMAINE
 destination_mail = config.ADRESSE_MAIL
 
 
-@bot.command(name='new')
-async def _new(ctx, source_mail):
-    client.post('/email/domain/{}/redirection'.format(domaine), **{
-        'from': source_mail + "@" + domaine,
-        'localCopy': False,
-        'to': destination_mail,
-    })
-    await ctx.send(f"L'alias : {source_mail} a bien été créé.")
-
-
-@bot.command(name="del")
-async def _del(ctx, source_mail):
-    liste_redirections = client.get(f'/email/domain/{domaine}/redirection')
-    for i in liste_redirections:
-        redirection = client.get(f'/email/domain/{domaine}/redirection/{i}')
-        if redirection['from'] == source_mail + "@" + domaine:
-            redirection_id = redirection["id"]
-            supprimer = client.delete(f'/email/domain/{domaine}/redirection/{redirection_id}')
-            await ctx.send(f"L'alias : {supprimer['account']} a bien été supprimé.")
-            break
-
-
-@bot.command(name="maj")
-async def _maj(ctx):
+async def maj(ctx):
     liste = []
     await bot.get_channel(ctx.channel.id).purge(limit=None)
-    await ctx.send(f"Dernière MAJ le {datetime.datetime.now().strftime('%d/%m/%Y')}")
+    await ctx.send(f"MAJ le {datetime.datetime.now().strftime('%d/%m/%Y')}")
     liste_redirections = client.get(f'/email/domain/{domaine}/redirection')
     for i in liste_redirections:
         redirection = client.get(f'/email/domain/{domaine}/redirection/{i}')
@@ -50,6 +29,43 @@ async def _maj(ctx):
     liste.sort()
     for i in liste:
         await ctx.send(i)
+    await ctx.send("-" * 30)
+
+
+@bot.command(name='new')
+async def _new(ctx, source_mail):
+    try:
+        client.post('/email/domain/{}/redirection'.format(domaine), **{
+            'from': source_mail + "@" + domaine,
+            'localCopy': False,
+            'to': destination_mail,
+        })
+        await maj(ctx)
+        await ctx.send(f"🆕 L'alias : {source_mail} a bien été créé.")
+    except ResourceConflictError:
+        await ctx.send(f"⚠️ L'alias : {source_mail} existe déjà.")
+
+
+@bot.command(name="del")
+async def _del(ctx, source_mail):
+    existe = False
+    liste_redirections = client.get(f'/email/domain/{domaine}/redirection')
+    for i in liste_redirections:
+        redirection = client.get(f'/email/domain/{domaine}/redirection/{i}')
+        if redirection['from'] == source_mail + "@" + domaine:
+            supprimer = client.delete(f'/email/domain/{domaine}/redirection/{redirection["id"]}')
+            await maj(ctx)
+            await ctx.send(f"🗑 L'alias : {supprimer['account']} a bien été supprimé.")
+            existe = True
+            break
+    if not existe:
+        await maj(ctx)
+        await ctx.send(f"🚫 L'alias : {source_mail} n'existe pas.")
+
+
+@bot.command(name="maj")
+async def _maj(ctx):
+    await maj(ctx)
 
 
 @bot.command(name="clear")
